e17a6e32e4a9784da5372223138f5c5dd5c9779c
WataruHigaki-web <wataruhigaki@gmail.com>
2020-03-19 11:31:59 +0000

    add model and column except for Q&A,add rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,40 @@
 Rails.application.routes.draw do
+
+  root 'users/products#top'
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  namespace :admins do
+    root 'orders#top'
+    get 'products/set_index'
+    get 'products/set_show'
+    resources :users,        only:[:index,:show,:edit,:update]
+    resources :products
+    resources :orders,       only:[:index,:show,:update]
+    resources :order_records,only:[:index,:show,:update]
+    resources :product_kinds
+    resources :admins,       only:[:index,:show,:edit,:update]
+  end
+
+  namespace :users do
+    get 'users/point'
+    get 'users/confirm'
+    get 'orders/confirm'
+    resources :users,        only:[:show,:edit,:update]
+    resources :products,     only:[:index,:show]
+    resources :orders,       only:[:new,:create]
+    resources :cart_items,   only:[:index,:edit,:update,:destroy,:create]
+    resources :creditcards
+    resources :favorites,    only:[:index,:show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,148 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_19_113029) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "name_kana"
+    t.string "profile_image_id"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.integer "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "creditcards", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "credit_number"
+    t.string "credit_name"
+    t.integer "security_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "in_points", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "order_id"
+    t.integer "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_records", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.integer "day"
+    t.integer "end_price"
+    t.boolean "status", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "get_status"
+    t.integer "return_status"
+    t.integer "create_point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "out_points", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "order_id"
+    t.integer "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_kinds", force: :cascade do |t|
+    t.string "name"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "product_kind_id"
+    t.string "name"
+    t.text "introduction"
+    t.integer "price"
+    t.string "product_image_id"
+    t.integer "stock"
+    t.boolean "status", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "name_kana"
+    t.string "profile_image_id"
+    t.string "zip_code"
+    t.string "address"
+    t.string "phone_number"
+    t.integer "credit_number"
+    t.string "credit_name"
+    t.integer "security_code"
+    t.boolean "status", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
