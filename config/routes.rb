eb55e47c36588518e7bab134ae116fe0b70c6e18
WataruHigaki-web <wataruhigaki@gmail.com>
2020-04-17 06:21:25 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -85,5 +85,5 @@ Rails.application.routes.draw do
   end
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
